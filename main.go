1f5c782eeafc5b9d81f4bebb0f02cf84665a9e1b
Vsevolod <skimin1992@hotmail.com>
2026-02-13 15:22:17 +0400

    Readiness Endpoint Added

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
@@ -14,7 +20,9 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	// Ensure the program loggs an error if encountered with server listening failure
 	log.Fatal(srv.ListenAndServe())
