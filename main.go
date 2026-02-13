def21c837f26230ee4872632a6d9bbf0bb6247cc
Vsevolod <skimin1992@hotmail.com>
2026-02-13 16:52:45 +0400

    Stateful Config and Middleware Pattern with Incrementing Counter Added

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+// stateful config
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// middleware pattern that wraps handlers and increments the counter
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -11,9 +26,24 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+// handler that writes the number of request that have been counted
+func (cfg *apiConfig) handlerCount(w http.ResponseWriter, r *http.Request) {
+	val32 := cfg.fileserverHits.Load()
+	str := fmt.Sprintf("Hits: %d", val32)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(str))
+}
+
+// handler that resets apiConfig count to 0
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -21,9 +51,11 @@ func main() {
 	}
 
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerCount)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-	// Ensure the program loggs an error if encountered with server listening failure
+	// ensure the program loggs an error if encountered with server listening failure
 	log.Fatal(srv.ListenAndServe())
 }
