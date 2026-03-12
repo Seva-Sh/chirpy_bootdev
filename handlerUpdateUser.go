0cf6ba122960c741dc9683b0c8868d8490cc88ef
Seva-Sh <skimin1992@hotmail.com>
2026-03-12 21:36:48 +0400

    User Password and Email Update Functionality Added

---
+++ handlerUpdateUser.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Seva-Sh/chirpy_bootdev/internal/auth"
+	"github.com/Seva-Sh/chirpy_bootdev/internal/database"
+)
+
+func (cfg *apiConfig) handlerUpdateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Password string `json:"password"`
+		Email    string `json:"email"`
+	}
+
+	// decode request body for params
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error decoding parameters")
+		return
+	}
+
+	// get the token
+	accessToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not retrieve token")
+		return
+	}
+
+	// get the user
+	userID, err := auth.ValidateJWT(accessToken, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "The given token is invalid")
+		return
+	}
+
+	// hash the password
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failure hashing the password")
+		return
+	}
+
+	// update user info
+	user, err := cfg.db.UpdateUser(r.Context(), database.UpdateUserParams{
+		ID:             userID,
+		Email:          params.Email,
+		HashedPassword: hashedPassword,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failure updating the user")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	})
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -8,3 +8,9 @@ VALUES (
     $2
 )
 RETURNING *;
+
+-- name: UpdateUser :one
+UPDATE users
+SET email = $2, hashed_password = $3, updated_at = NOW()
+WHERE id = $1
+RETURNING *;
